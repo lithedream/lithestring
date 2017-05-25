31fa10d592787783a5f86fc0205fbce23c01e0c2
lithedream <lithedream@desktop>
2017-05-25 02:39:12 +0200

    Zip/Unzip type 1 & 3 implemented

--- src/lithe/core/LitheString.java
+++ src/lithe/core/LitheString.java
@@ -2,6 +2,8 @@ package lithe.core;
 
 import java.io.*;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 
 public class LitheString {
@@ -39,26 +41,26 @@ public class LitheString {
         BitWriter output = new BitWriter();
         output.write01("100");
 
-        try (PushbackInputStream bais = new PushbackInputStream(new ByteArrayInputStream(input.getBytes(Charset.forName("UTF-8"))))) {
+        try (PushbackInputStream bais = new PushbackInputStream(new ByteArrayInputStream(input.getBytes(StandardCharsets.UTF_8)))) {
             boolean caps = false;
             byte byt;
             while ((byt = (byte) bais.read()) != -1) {
                 if (byt >= 97 && byt <= 122) { // lower
                     if (caps) {
-                        output.write01("11011");
+                        output.write01("00000");
                         caps = !caps;
                     }
-                    output.writeLast5Bits((byte) (byt - 97));
+                    output.writeLast5Bits((byte) (byt - 96));
 
                 } else if (byt >= 65 && byt <= 90) { // upper
                     if (!caps) {
-                        output.write01("11011");
+                        output.write01("00000");
                         caps = !caps;
                     }
-                    output.writeLast5Bits((byte) (byt - 65));
+                    output.writeLast5Bits((byte) (byt - 64));
 
                 } else if (byt == 32) { //space
-                    output.write01("11010");
+                    output.write01("11011");
                 } else {
 
                     int nExtraByte = 0;
@@ -112,11 +114,11 @@ public class LitheString {
 
     }
 
-    private static byte[] z3(String input) {
+    public static byte[] z3(String input) {
         ByteArrayOutputStream output = new ByteArrayOutputStream();
         output.write(0b10111111);
         try {
-            try (Writer writer = new OutputStreamWriter(new GZIPOutputStream(output), Charset.forName("UTF-8"))) {
+            try (Writer writer = new OutputStreamWriter(new GZIPOutputStream(output), StandardCharsets.UTF_8)) {
                 writer.write(input);
             }
         } catch (IOException e) {
@@ -133,11 +135,82 @@ public class LitheString {
     }
 
     public static String unzip(byte[] content) {
-        //TODO
-        return null;
+        if (content == null) {
+            return null;
+        }
+        if (content.length == 0) {
+            return "";
+        }
+        if (startsWith(content[0], "100")) {
+            return unzip1(content);
+        }
+        if ((content[0] & 0xFF) == 0b10111111) {
+            return unzip3(content);
+        }
+        return new String(content, StandardCharsets.UTF_8);
     }
 
-    public static byte[] shortest(byte[]... bytes) {
+    private static String unzip3(byte[] content) {
+        ByteArrayInputStream bais = new ByteArrayInputStream(content);
+        bais.read();
+        try (GZIPInputStream gis = new GZIPInputStream(bais)) {
+            byte[] buffer = new byte[1024];
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+            int len;
+            while ((len = gis.read(buffer)) > 0) {
+                out.write(buffer, 0, len);
+            }
+
+            gis.close();
+            out.close();
+            return new String(out.toByteArray(), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            throw new IllegalArgumentException(e);
+        }
+
+    }
+
+    private static String unzip1(byte[] content) {
+        BitReader bitReader = new BitReader(content);
+        bitReader.advance(3);
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        boolean caps = false;
+        while (!bitReader.isClosed()) {
+            if (bitReader.peek01("111")) {
+                bitReader.advance(3);
+                byte read = bitReader.read();
+
+                int nExtraByte = 0;
+                if (!startsWith(read, "0")) { //1 byte
+                    if (startsWith(read, "110")) { //2 byte
+                        nExtraByte = 1;
+                    } else if (startsWith(read, "1110")) { //3 byte
+                        nExtraByte = 2;
+                    } else if (startsWith(read, "11110")) { //4 byte
+                        nExtraByte = 3;
+                    }
+                }
+                baos.write(read);
+                for (int i = 0; i < nExtraByte; i++) {
+                    baos.write(bitReader.read());
+                }
+            } else {
+                byte read = bitReader.read(5);
+                if (read == 0) {
+                    caps = !caps;
+                } else if ((read & 0xFF) == 0b00011011) {
+                    baos.write(32); // space
+                } else {
+                    baos.write(read + (caps ? 64 : 96)); // UPPER:lower
+                }
+            }
+        }
+        return new String(baos.toByteArray(), StandardCharsets.UTF_8);
+    }
+
+    private static byte[] shortest(byte[]... bytes) {
         byte[] shortest = bytes[0];
         for (int i = 1; i < bytes.length; i++) {
             if (bytes[i].length < shortest.length) {
@@ -146,53 +219,6 @@ public class LitheString {
         }
         return shortest;
     }
-/*
-
-    private static class BitWriter {
-        private final BitSet b;
-        private int counter = 0;
-
-        public BitWriter(BitSet b) {
-            this.b = b;
-        }
-
-        public void write(boolean bit) {
-            b.set(counter++, bit);
-        }
-
-        public void write01(String binaryString) {
-            char[] chars = binaryString.toCharArray();
-            for (char c : chars) {
-                b.set(counter++, c == '1');
-            }
-        }
-
-
-        public void writeChar(char c) {
-            byte[] bytes = new String(new char[]{c}).getBytes(Charset.forName("UTF-8"));
-            write(bytes);
-        }
-
-        public void write(byte value) {
-            for (int bit = 0; bit < 7; bit++) {
-                b.set(counter++, (value & (1 << bit)) != 0);
-            }
-        }
-
-        public void write(byte[] values) {
-            for (byte value : values) {
-                write(value);
-            }
-        }
-
-        public byte[] toByteArray() {
-            return b.toByteArray();
-        }
-
-
-    }
-    */
-
 
     private static class BitWriter {
         private static final int START = 7;
@@ -228,12 +254,6 @@ public class LitheString {
             }
         }
 
-/*
-        public void writeChar(char c) {
-            byte[] bytes = new String(new char[]{c}).getBytes(Charset.forName("UTF-8"));
-            write(bytes);
-        }*/
-
         public void write(byte value) {
             for (byte bit = 8; bit-- > 0; ) {
                 write(getNBitValue(value, bit));
@@ -243,12 +263,6 @@ public class LitheString {
         public void writeLast5Bits(byte value) {
             for (byte bit = 5; bit-- > 0; ) {
                 write(getNBitValue(value, bit));
-            }
-        }
-
-        public void write(byte[] values) {
-            for (byte value : values) {
-                write(value);
             }
         }
 
@@ -277,53 +291,105 @@ public class LitheString {
 
     }
 
-    //TODO
     private static class BitReader {
-        private static final int START = 7;
-        private final ByteArrayOutputStream b = new ByteArrayOutputStream();
-        private boolean closed = false;
-        private byte current = 0;
-        private int curpos = START;
-        private ByteArrayInputStream bais;
+        private static final byte START = 7;
+        private byte curpos = START;
+        private byte[] bytes;
+        private int bytePos = 0;
 
         public BitReader(byte[] b) {
-            bais = new ByteArrayInputStream(b);
-            privateRead();
-        }
-
-        private void privateRead() {
-            int read = bais.read();
-            if (read == -1) {
-                closed = true;
-                current = 0;
-            } else {
-                current = (byte) read;
-            }
+            bytes = b;
         }
 
         public byte read() {
             if (curpos == START) {
-                byte toRet = current;
-                privateRead();
+                if (bytePos >= bytes.length) {
+                    return 0; //force return 0 because the stream ends
+                }
+                byte toRet = bytes[bytePos];
+                bytePos++;
                 return toRet;
             } else {
+                byte toRet = 0;
                 for (int bit = 8; bit-- > 0; ) {
-
+                    if (bytePos >= bytes.length) {
+                        return 0; //force return 0 because the stream ends
+                    }
+                    if (getNBitValue(bytes[bytePos], curpos)) {
+                        toRet |= 1 << bit;
+                    } else {
+                        toRet &= ~(1 << bit);
+                    }
+                    if (curpos == 0) {
+                        bytePos++;
+                        curpos = START;
+                    } else {
+                        curpos--;
+                    }
                 }
+                return toRet;
 
             }
+        }
 
-
-            return 0;
-
+        public byte read(int numberOfBits) {
+            byte toRet = 0;
+            for (int bit = numberOfBits; bit-- > 0; ) {
+                if (bytePos >= bytes.length) {
+                    return 0; //force return 0 because the stream ends
+                }
+                if (getNBitValue(bytes[bytePos], curpos)) {
+                    toRet |= 1 << bit;
+                } else {
+                    toRet &= ~(1 << bit);
+                }
+                if (curpos == 0) {
+                    bytePos++;
+                    curpos = START;
+                } else {
+                    curpos--;
+                }
+            }
+            return toRet;
         }
 
         public boolean isClosed() {
-            return closed;
+            return bytePos >= bytes.length;
+        }
+
+        public void advance(int n) {
+            for (int i = 0; i < n; i++) {
+                if (curpos == 0) {
+                    bytePos++;
+                    curpos = START;
+                } else {
+                    curpos--;
+                }
+            }
         }
 
         public boolean peek01(String s) {
-            return false;
+            int bytePosCopy = bytePos;
+            byte curPosCopy = curpos;
+
+            for (int i = 0; i < s.length(); i++) {
+                if (bytePosCopy >= bytes.length) {
+                    return false;
+                }
+                boolean bitValue = getNBitValue(bytes[bytePosCopy], curPosCopy);
+                boolean iBitIsSet = s.charAt(i) == ('1');
+                if (bitValue != iBitIsSet) {
+                    return false;
+                }
+
+                if (curPosCopy == 0) {
+                    bytePosCopy++;
+                    curPosCopy = START;
+                } else {
+                    curPosCopy--;
+                }
+            }
+            return true;
         }
     }
 
